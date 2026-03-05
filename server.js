f1f91c3d1c0cef44b8b0092dc0513df84a765f4f
Mdibrahimweb <lsp2020e@gmail.com>
2026-03-05 21:53:15 +0600

    personal setu p

--- server.js
+++ server.js
@@ -4,50 +4,53 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http, { maxHttpBufferSize: 1e8 });
 const mongoose = require('mongoose');
 
-// MongoDB URI
 const uri = "mongodb+srv://nid-server:Ibrahim9250@cluster0.9jxg3wa.mongodb.net/messengerDB?retryWrites=true&w=majority";
 
 mongoose.connect(uri)
-    .then(() => console.log("✅ MongoDB Connected!"))
-    .catch(err => console.error("❌ MongoDB Error:", err));
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
 
-// Schema
 const messageSchema = new mongoose.Schema({
-    user: String,
-    text: String,
-    time: String,
-    isFile: Boolean,
-    msgId: String,
-    avatar: String
+user:String,
+text:String,
+time:String,
+isFile:Boolean,
+msgId:String,
+avatar:String
 });
-const Message = mongoose.model('Message', messageSchema);
 
-app.use(express.static('public'));
+const Message = mongoose.model("Message",messageSchema);
 
-io.on('connection', async (socket) => {
-    // Load old messages
-    const history = await Message.find().sort({ _id: 1 }).limit(100);
-    socket.emit('load-history', history);
+app.use(express.static("public"));
 
-    socket.on('new-user', data => {
-        socket.broadcast.emit('user-joined', data.name);
-    });
+io.on("connection",async(socket)=>{
 
-    socket.on('chat-message', async (data) => {
-        const newMessage = new Message(data);
-        await newMessage.save();
-        socket.broadcast.emit('chat-message', data);
-    });
+const history = await Message.find().sort({_id:1}).limit(100);
+socket.emit("load-history",history);
 
-    socket.on('delete-message', async (msgId) => {
-        await Message.deleteOne({ msgId: msgId });
-        io.emit('message-deleted', msgId);
-    });
+socket.on("new-user",(data)=>{
+socket.broadcast.emit("user-joined",data.name);
+});
 
-    socket.on('typing', (data) => {
-        socket.broadcast.emit('display-typing', data);
-    });
+socket.on("chat-message",async(data)=>{
+const msg = new Message(data);
+await msg.save();
+socket.broadcast.emit("chat-message",data);
+});
+
+socket.on("delete-message",async(id)=>{
+await Message.deleteOne({msgId:id});
+io.emit("message-deleted",id);
+});
+
+socket.on("typing",(data)=>{
+socket.broadcast.emit("display-typing",data);
+});
+
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`🚀 Server: http://localhost:${PORT}`));
+
+http.listen(PORT,()=>{
+console.log("Server running on http://localhost:"+PORT);
+});
